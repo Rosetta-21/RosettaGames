5dacde37ba87fd43d6153767dacdcba2657c01ee
Ifarra <ifarra1324@gmail.com>
2024-03-10 19:32:19 +0700

    light mode home

--- asset/js/theme.js
+++ asset/js/theme.js
@@ -1,5 +1,6 @@
 const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const themePreference = localStorage.getItem('theme');
+const themeicon = document.getElementById('icon-ctrl');
 
 if (themePreference === 'dark' || (themePreference === null && prefersDarkTheme)) {
   document.documentElement.setAttribute('data-theme', 'dark');
@@ -10,6 +11,14 @@ if (themePreference === 'dark' || (themePreference === null && prefersDarkTheme)
 function toggleTheme(){
   const currentTheme = document.documentElement.getAttribute('data-theme');
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  if (themeicon.classList.contains('fa-moon')){
+    themeicon.classList.remove('fa-moon');
+    themeicon.classList.add('fa-sun');
+  } else {
+    themeicon.classList.remove('fa-sun');
+    themeicon.classList.add('fa-moon');
+  }
+
   document.documentElement.setAttribute('data-theme', newTheme);
   localStorage.setItem('theme', newTheme);
 }
